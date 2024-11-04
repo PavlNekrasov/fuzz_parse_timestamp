6a21dd9580e479da3d70078b06587f9e2fa7661b
PavlNekrasov <95914807+PavlNekrasov@users.noreply.github.com>
2024-11-04 14:28:29 +0300

    Update build.sh

--- artifacts/build.sh
+++ artifacts/build.sh
@@ -9,13 +9,11 @@ cd "$SYSTEMD_DIR"
 CC=clang CXX=clang++ meson setup "$BUILD_DIR"  -Dllvm-fuzz=true -Db_sanitize=address,undefined -Db_lundef=false
 cd "$BUILD_DIR" && ninja
 
-cd /home/fuzz/artifacts
-mv "$SOURCE_FILE" ./../
-cd ./..
+cd /home/fuzz/
 mkdir corpus
 
 
-clang -fsanitize=fuzzer,address,undefined -g "$SOURCE_FILE" -o fuzz \
+clang -fsanitize=fuzzer,address,undefined -g "./artifacts/$SOURCE_FILE" -o fuzz \
     -I "$SYSTEMD_DIR/src/basic/" -I "$SYSTEMD_DIR/src/fundamental/" -I "$BUILD_DIR/" \
     "$BUILD_DIR/src/basic/libbasic.a" "$BUILD_DIR/src/shared/libsystemd-shared-249.a"
 
